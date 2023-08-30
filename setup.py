954b02f95a4e7f6613d16ab5b4c47dd9ebbfea8c
Mark Phelps <209477+markphelps@users.noreply.github.com>
2023-08-30 12:51:43 -0400

    chore: idk setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+setup(
+   name='flipt-migrate',
+   version='0.1.0',
+   author='Flipt Devs',
+   author_email='dev@flipt.io',
+   description = "A CLI tool to migrate feature flags from one source (e.g. competitor) to Flipt",
+   packages=find_packages(),
+   entry_points={
+      'console_scripts': [
+         'flipt-migrate=flipt_migrate.cli:main',
+      ],
+   },
+)
